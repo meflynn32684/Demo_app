2b800fa7d6b611396463c56578ddd8294effab8e
Mike Flynn <meflynn32684@gmail.com>
2012-07-29 16:38:58 -0700

    Finished layout and routes for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 DemoApp::Application.routes.draw do
+
+  match '/contact', :to=>'pages#contact'
+  match '/about', :to=>'pages#about'
+  match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'
+
+  root :to => 'pages#home'
+
+  get "users/new"
+
   get "pages/home"
 
   get "pages/contact"
